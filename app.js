795986a4af9867cd071ddcaba6c4e0dcb2dc81ed
Scottlexium <scottlexium@gmail.com>
2022-02-25 17:27:41 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express()
+const port = 4040
+const bodyparser = require('body-parser');
+
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));;
+app.set('views', 'views');
+
+app.get('/', (req, res)=>{
+    res.render('index');
+})
+app.get('/about', (req, res)=>{
+    res.render('about');
+});
+app.get('/contact_us', (req, res)=>{
+    res.render('contact_us');
+});
+app.use('*', (req, res)=>{
+    res.status(404);
+    res.render('404');
+})
+app.listen(port, () => console.log(`Listening on http://localhost:${port}`))
