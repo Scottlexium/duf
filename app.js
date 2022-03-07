75c1434e5a6a68375ec7b38e7ed7f0a879c802eb
scottlexium <scottlexium@gmail.com>
2022-03-07 16:27:43 +0100

    oj

--- app.js
+++ app.js
@@ -8,17 +8,20 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));;
 app.set('views', 'views');
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.render('index');
 })
-app.get('/about', (req, res)=>{
+app.get('/about', (req, res) => {
     res.render('about');
 });
-app.get('/contact_us', (req, res)=>{
+app.get('/contact_us', (req, res) => {
     res.render('contact_us');
 });
-app.use('*', (req, res)=>{
+app.use('*', (req, res) => {
     res.status(404);
     res.render('404');
+})
+app.get('/favicon.ico', (req, res) => {
+    res.status(200);
 })
 app.listen(port, () => console.log(`Listening on http://localhost:${port}`))
