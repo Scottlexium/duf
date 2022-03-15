5c212d3d372d10a5789b1e545b1172d3b969b4b1
scottlexium <scottlexium@gmail.com>
2022-03-15 16:26:33 +0100

    aot

---
+++ public/js/preloader.js
@@ -0,0 +1,8 @@
+
+window.addEventListener('load', (e)=>{
+    const body = document.querySelector('body');
+    const loader = document.getElementById('preload');
+  loader.style.display = "none";
+  body.style.overflowY = 'auto';
+
+})
