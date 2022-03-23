f170fa7fa182d532191f92c9bea2c788c3e617af
scottlexium <scottlexium@gmail.com>
2022-03-24 00:13:09 +0100

    c

--- app.js
+++ app.js
@@ -69,6 +69,7 @@ app.post('/contact_post',(req, res)=>{
             res.status(200).json({err:'An error occured!'})
         } else {
             console.log(info)
+            smtpError = "Message sent Successfully!";
             res.status(200).json({msg:smtpError})
         }
         
